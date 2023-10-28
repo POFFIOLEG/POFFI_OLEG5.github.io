9ffb361540f5d360581a0f30c098ba3799fbd91b
poffioleg <144158430+POFFIOLEG@users.noreply.github.com>
2023-10-28 20:19:08 +0300

    45

--- y/yr/js.js
+++ y/yr/js.js
@@ -4,3 +4,12 @@ function calculate() {
     var result = quantity * product;
     alert("Стоимость заказа: " + result + " рублей");
    }
+
+
+   $(".lazy").slick({
+    lazyLoad: "ondemand",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500
+  });
+  
